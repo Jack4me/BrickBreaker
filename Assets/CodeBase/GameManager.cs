b05f1b13723f66e86b5eeaa48f571b4f217c8d1c
Jack4me <just4go@gmail.com>
2024-02-29 14:28:07 +0100

    +3

--- Assets/CodeBase/GameManager.cs
+++ Assets/CodeBase/GameManager.cs
@@ -1,11 +1,35 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 namespace CodeBase
 {
     public class GameManager : MonoBehaviour
     {
-  
-        
-        
+        [SerializeField] private int score;
+        [SerializeField] private int lives;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void Start()
+        {
+            NewGame();
+        }
+
+        private void NewGame()
+        {
+            score = 0;
+            lives = 3;
+            LoadLevel(1);
+        }
+
+        public void LoadLevel(int level)
+        {
+            SceneManager.LoadScene("Level" + level);
+        }
     }
 }
