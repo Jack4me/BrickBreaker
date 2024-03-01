f76898dbaf8181e5e89bdb634683faa52fb7350b
Jack4me <just4go@gmail.com>
2024-03-01 18:19:37 +0100

    +8

--- Assets/CodeBase/Brick.cs
+++ Assets/CodeBase/Brick.cs
@@ -1,9 +1,22 @@
+using System;
 using UnityEngine;
 
 namespace CodeBase {
-    public class Brick : MonoBehaviour
-    {
-    
-    
+    public class Brick : MonoBehaviour {
+        public int Health;
+        public SpriteRenderer Sprite;
+        public Sprite[] States;
+        public bool unbreakeble;
+        private void Awake() {
+            Sprite = GetComponent<SpriteRenderer>();
+        }
+
+        private void Start() {
+            if (!unbreakeble)
+            {
+                Health = States.Length;
+                Sprite.sprite = States[Health - 1];
+            }
+        }
     }
 }
